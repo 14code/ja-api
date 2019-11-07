cfeb018d1afa14968777751f3ab92ee06549a96b
David Hoppe <dh@smpmedia.net>
2019-11-07 16:16:06 +0100

    Add example controller to return submitted payload

---
+++ src/Controllers/ReturnPayloadController.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace I4code\JaApi\Controllers;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+
+class ReturnPayloadController
+{
+
+    public function __invoke(ServerRequestInterface $request, RequestHandlerInterface $handler)
+    {
+        $body = (string) $request->getBody();
+        echo $body;
+    }
+
+}
